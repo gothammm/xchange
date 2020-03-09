6e531be840033de28fc2689e179e8686fb50dbc0
Gautham Ramachandran <gautham2r@gmail.com>
2020-03-09 19:11:05 +0100

    Spec changes

--- src/pages/ExchangePage.tsx
+++ src/pages/ExchangePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import CenterPageContainer from '../components/CenterPageContainer';
 import { useWallet } from '../hooks/WalletHook';
 import CurrencyInput from '../components/CurrencyInput';
